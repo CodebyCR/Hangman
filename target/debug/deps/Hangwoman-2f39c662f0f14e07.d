20a28fcb8659919e3f394e744fe5daeb8bb1f3bb
Christoph Rohde <44606665+CodebyCR@users.noreply.github.com>
2023-07-20 10:40:05 +0200

    Delete unnecessary target folder

--- target/debug/deps/Hangwoman-2f39c662f0f14e07.d
+++
@@ -1,5 +0,0 @@
-/Users/christoph_rohde/Hangwoman/Hangwoman/target/debug/deps/Hangwoman-2f39c662f0f14e07.rmeta: src/main.rs
-
-/Users/christoph_rohde/Hangwoman/Hangwoman/target/debug/deps/Hangwoman-2f39c662f0f14e07.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/Hangwoman-e42b98fb32132786.d
+++
@@ -1,5 +0,0 @@
-/Users/christoph_rohde/Hangwoman/Hangwoman/target/debug/deps/Hangwoman-e42b98fb32132786.rmeta: src/main.rs
-
-/Users/christoph_rohde/Hangwoman/Hangwoman/target/debug/deps/Hangwoman-e42b98fb32132786.d: src/main.rs
-
-src/main.rs:
